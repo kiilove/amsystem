f8ba079f0feb7240fd259d47bfc469fef759d305
kiilove <jbkim@jncore.com>
2022-08-26 16:52:55 +0900

    commit on "main"

--- src/components/Tables.jsx
+++ src/components/Tables.jsx
@@ -1,89 +1,65 @@
 import React from "react";
 
-const Tables = () => {
+const Tables = (props) => {
   return (
-    <div className="flex flex-col w-full">
-      <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
-        <div class="px-6 inline-block min-w-full md:px-0 lg:px-4">
-          <div class="overflow-hidden p-5">
-            <table class="w-full">
-              <thead class="bg-white border-b">
-                <tr>
-                  <th
-                    scope="col"
-                    class="text-sm font-medium text-gray-900 px-6 py-4 text-left"
-                  >
-                    #
-                  </th>
-                  <th
-                    scope="col"
-                    class="text-sm font-medium text-gray-900 px-6 py-4 text-left"
-                  >
-                    관리번호
-                  </th>
-                  <th
-                    scope="col"
-                    class="text-sm font-medium text-gray-900 px-6 py-4 text-left"
-                  >
-                    만료일자
-                  </th>
-                  <th
-                    scope="col"
-                    class="text-sm font-medium text-gray-900 px-6 py-4 text-left"
-                  >
-                    사용자
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr class="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
-                  <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                    1
-                  </td>
-                  <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                    DT-20210901-001
-                  </td>
-                  <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                    2022-09-01
-                  </td>
-                  <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                    김진배
-                  </td>
-                </tr>
-                <tr class="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
-                  <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                    2
-                  </td>
-                  <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                    DT-20210901-002
-                  </td>
-                  <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                    2022-09-01
-                  </td>
-                  <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                    유한준
-                  </td>
-                </tr>
-                <tr class="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
-                  <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                    3
-                  </td>
-                  <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                    DT-20210901-003
-                  </td>
-                  <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                    2022-09-01
-                  </td>
-                  <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                    창고
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </div>
-      </div>
-    </div>
+    <table className="w-full">
+      <thead className="bg-white border-b">
+        <tr>
+          {props.header.map((hItem, idx) => (
+            <th
+              scope="col"
+              className="text-sm font-medium text-gray-900 px-6 py-4 text-left"
+            >
+              {hItem.text}
+            </th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        <tr className="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
+          <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+            1
+          </td>
+          <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+            DT-20210901-001
+          </td>
+          <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+            2022-09-01
+          </td>
+          <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+            김진배
+          </td>
+        </tr>
+        <tr className="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
+          <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+            2
+          </td>
+          <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+            DT-20210901-002
+          </td>
+          <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+            2022-09-01
+          </td>
+          <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+            유한준
+          </td>
+        </tr>
+        <tr className="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
+          <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+            3
+          </td>
+          <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+            DT-20210901-003
+          </td>
+          <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+            2022-09-01
+          </td>
+          <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+            창고
+          </td>
+        </tr>
+      </tbody>
+    </table>
   );
 };
 
--- src/pages/Assets.jsx
+++ src/pages/Assets.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import Tables from "../components/Tables";
 
 const Assets = () => {
+  const tableContent = {
+    header: ["#", "관리번호", "모델명", "사용자", "위치"],
+  };
   return (
     <div id="Container" className="flex w-full h-full space-10 flex-col p-4">
       <div className="flex flex-auto w-full h-full min-h-full bg-white rounded-md shadow-md p-5 flex-col">
         <div className="flex justify-start items-center w-full mb-5 ">
           <h1 className="text-sm font-semibold text-gray-700">전체자산</h1>
         </div>
-        <div className="flex">
+        <div className="flex min-w-full">
           <Tables />
         </div>
       </div>
